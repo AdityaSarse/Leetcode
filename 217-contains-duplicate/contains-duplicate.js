e37b2432050e7b070dc96838e149e1ac876eeb96
Aditya Sarse <adityasarse362@gmail.com>
2026-09-16 23:46:23 +0530

    Time: 31 ms (30.64%) | Memory: 79.3 MB (58.31%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const mp = new Map();
+    for(let i = 0  ; i < nums.length ; i++){
+        if(mp.has(nums[i])){
+            return true;
+        }
+        mp.set(nums[i],i);
+    }
+    return false;
+    
+};
